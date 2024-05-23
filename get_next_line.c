d1502eed334a54c41886e75d74a628ab1ce9fcbd
frenzla <alex.larose.sf@gmail.com>
2024-05-23 18:04:10 +0200

    finished - must debbug

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: alarose <alarose@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 09:29:14 by alarose           #+#    #+#             */
-/*   Updated: 2024/05/18 11:19:04 by alarose          ###   ########.fr       */
+/*   Updated: 2024/05/18 13:25:39 by alarose          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ int	add_to_stock(char c, t_list **stock)
 		return (-1);
 	new->c = c;
 	new->next = NULL;
-	if (!*stock)
+	if (!(*stock))
 		*stock = new;
 	else
 	{
@@ -58,10 +58,9 @@ int	add_to_stock(char c, t_list **stock)
 	return (0);
 }
 
-size_t	read_n_stock(int fd, char *buff)
+size_t	read_n_stock(int fd, char *buff, t_list **stock)
 {
 	size_t	ret_read;
-	t_list	*stock;
 	int		i;
 
 	ret_read = read(fd, buff, BUFFER_SIZE);
@@ -72,39 +71,104 @@ size_t	read_n_stock(int fd, char *buff)
 	i = 0;
 	while (i < ret_read)
 	{
-		add_to_stock(buff[i], &stock);
+		add_to_stock(buff[i], stock);
 		i++;
 	}
-	//WORKING: print_lst(&stock);
+	/*WORKING:*/ print_lst(stock);
 	return(ret_read);
+}
+
+int	find_NL_or_EOF(t_list **stock, int	ret_read)
+{
+	t_list	*tmp;
+
+	tmp = *stock;
+	while (tmp)
+	{
+		if (ret_read < BUFFER_SIZE || tmp->c == '\n')
+		{
+			printf("NL or EOF found\n");
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+	return (0);
+}
+size_t	get_len(t_list **stock)
+{
+	t_list	*tmp;
+	size_t	nb_chars;
+
+	tmp = *stock;
+	nb_chars = 0;
+	while (tmp->c != '\n' && tmp)
+	{
+		nb_chars++;
+		tmp = tmp->next;
+	}
+	if (tmp->c == '\n')
+		nb_chars++;
+	return (nb_chars);
+}
+
+char	*cpy_n_free(t_list **stock, size_t nb_chars)
+{
+	char	*line;
+	t_list	*tmp;
+	size_t		i;
+
+	line = malloc(sizeof(char) * (nb_chars + 1));
+	if (!line)
+		return (NULL);
+	i = 0;
+	printf("Copied: ");
+	while (i < nb_chars && *stock) // && *stock ?
+	{
+		line[i] = (*stock)->c;
+		printf("%c|", line[i]);
+		tmp = *stock;
+		*stock = (*stock)->next;
+		free(tmp);
+		i++;
+	}
+	line[i]= '\0';
+	return (line);
 }
 
 char	*get_next_line(int fd)
 {
 	//create buffer
-	char 	buff[BUFFER_SIZE +1];
-	size_t	ret_read;
+	char 			buff[BUFFER_SIZE + 1];
+	size_t			ret_read;
+	size_t			nb_chars;
+	static t_list	*stock;
 
 	ret_read = -1;
+	stock = NULL;
+	printf("CURRENT STOCK: ");
+	print_lst(&stock);
 	//read & stock
 	while(ret_read != 0)
 	{
-		ret_read = read_n_stock(fd, buff);
-		if (ret_read < 1)
-			return (NULL);
+		ret_read = read_n_stock(fd, buff, &stock);
 		//WORKING: printf("\nRET_READ = %zu\n\n", ret_read);
-		//check stock for newline OR EOF
-
+		//check stock for newline OR EOF => break while if NL OR EOF
+		if (find_NL_or_EOF(&stock, ret_read))
+			break;
 	}
+	if (ret_read == 0 || !stock)
+		return (NULL);
 	//count nb_chars to write in line
-
+	nb_chars = get_len(&stock);
+	printf("Chars to copy: %zu\n", nb_chars);
 	//copy in line and free stock
-	return (NULL);
+	return(cpy_n_free(&stock, nb_chars));
 }
 
 int	main(void)
 {
 	int	fd;
+	char *line;
 	//Open file
 	fd = open("./myExample.txt", O_RDONLY);
 	if (fd < 1)
@@ -116,8 +180,14 @@ int	main(void)
 		printf("File open\n" );
 
 	//call get_next_line in a loop
-	get_next_line(fd);
-
+	line = get_next_line(fd);
+	printf("LINE: %s\n", line);
+	printf("********************************\n");
+	while (line)
+	{
+		printf("LINE: %s\n", get_next_line(fd));
+		printf("********************************\n");
+	}
 	//free and close file
 	close (fd);
 
