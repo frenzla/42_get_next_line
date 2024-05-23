2a65aad51f5a415b337b7f0c9d0468f86b3ccdaa
frenzla <alex.larose.sf@gmail.com>
2024-05-23 18:02:17 +0200

    The basic function is working

--- sandbox.c
+++ sandbox.c
@@ -33,6 +33,8 @@ void	add_to_stock(t_list **lst, char c)
 	t_list	*new;
 	t_list	*temp;
 
+	if (!(*lst))
+		printf("STOCK EMPTY\n");
 	new = malloc(sizeof(t_list));
 	if (!new)
 		return ;
@@ -49,6 +51,35 @@ void	add_to_stock(t_list **lst, char c)
 	}
 }
 
+int	free_stock(t_list **lst, int nb_chars)
+{
+	int	i;
+	t_list	*tmp;
+	t_list	*next;
+
+	i = 0;
+		printf("Deleting from list: ");
+		while (i < nb_chars && *lst != NULL)
+		{
+			printf("%c", (*lst)->c);
+			printf("|");
+			next = (*lst)->next;
+			free(*lst);
+			*lst = next;
+			i++;
+		}
+		printf("\n");
+
+	i = 0;
+	tmp = *lst;
+	while (tmp != NULL)
+	{
+		i++;
+		tmp = tmp->next;
+	}
+	return (i);
+}
+
 int	get_next_line(int fd, char **line)
 {
 		int				ret_read;
@@ -56,9 +87,10 @@ int	get_next_line(int fd, char **line)
 		int				i;
 		int				k;
 		static t_list	*stock;
-		static int		nb_chars;
+		static int		nb_chars = 0;
 		t_list			*current;
 
+	printf("nb_chars = %d\n", nb_chars);
 //1. check if BUFFER_SIZE is def and not 0
 	if (BUFFER_SIZE < 1)
 	{
@@ -71,11 +103,11 @@ int	get_next_line(int fd, char **line)
 		return (1);
 //3. read and store BUFFER_SIZE nb of bytes in buff.
 	ret_read = -1;
-	nb_chars = 0;
 	i = BUFFER_SIZE;
-	while (i == BUFFER_SIZE)
+	while (i == BUFFER_SIZE && ret_read != 0)
 	{
 		ret_read = read(fd, buff, BUFFER_SIZE);
+		printf("ret de READ = %d\n", ret_read);
 		if (ret_read == -1)
 		{
 				printf("File couldn't READ: ");
@@ -85,18 +117,21 @@ int	get_next_line(int fd, char **line)
 	//4. check buff for \n
 		i = ft_strchr(buff, '\n');
 		nb_chars += i;
-		printf("**********************\n");
+/*		printf("**********************\n");
 		printf("buff size : %d\nchars to newline (10 = no NL): %d\ntotal nb_chars: %d\n", BUFFER_SIZE, i, nb_chars);
-		printf("**********************\n");
+		printf("**********************\n");*/
 		k = 0;
 	//5. write buff in stock
-		while (k < BUFFER_SIZE)
+		while (k < ret_read)
 		{
 			add_to_stock(&stock, buff[k]);
 			k++;
 		}
 		// ****************CHECK****************
 		current = stock;
+		if (current == NULL)
+			printf("STOCK F****G EMPTY");
+		printf("IN STOCK: ");
 		while (current != NULL)
 		{
 			printf("%c", current->c);
@@ -111,10 +146,10 @@ int	get_next_line(int fd, char **line)
 			//	write stock in line until /n
 			*line = malloc(sizeof(char) * (nb_chars + 1));
 			k = 0;
-			**line = 'A';
+/*			**line = 'A';
 			(*line)++;
 			**line = 'B';
-			(*line)--;
+			(*line)--;*/
 			current = stock;
 			while (k < nb_chars)
 			{
@@ -123,25 +158,53 @@ int	get_next_line(int fd, char **line)
 				k++;
 			}
 			(*line)[k] = 0;
+			printf("nb_chars = %d\n", nb_chars);
 			printf("Line written: %s\n", *line);
 		//	del what has been written from stock
-
+			// ****************CHECK****************
+			current = stock;
+			if (current == NULL)
+				printf("STOCK F****G EMPTY");
+			printf("IN STOCK: ");
+			while (current != NULL)
+			{
+				printf("%c", current->c);
+				current = current->next;
+			}
+			printf("\n");
+			// ****************CHECK****************
+			nb_chars = free_stock(&stock, nb_chars + 1);
+			// ****************CHECK****************
+			current = stock;
+			if (current == NULL)
+				printf("STOCK F****G EMPTY");
+			printf("Still remaining: ");
+			while (current != NULL)
+			{
+				printf("%c", current->c);
+				printf("|");
+				current = current->next;
+			}
+			// ****************CHECK****************
+		// free buff malloc
+			free(buff);
 		//	return
-
-			return (1);
+			if (i == 0)
+				return (0);
+			else
+				return (1);
 		}
 	}
-	return (0);
 }
 
 int	main(void)
 {
 	int		fd;
-	int		count;
+	int		ret;
 	char	**line;
 
 
-	count = 0;
+	ret = 0;
 	errno = 0;
 	line = malloc(sizeof(char *));
 //Open file
@@ -152,7 +215,14 @@ int	main(void)
 		print_error();
 		return (1);
 	}
-	get_next_line(fd, line);
+	ret = get_next_line(fd, line);
+	printf("\nRet de get_next_line: %d\n", ret);
+	printf("\n**********************************************\n\n");
+	ret = get_next_line(fd, line);
+	printf("\nRet de get_next_line: %d\n", ret);
+	printf("\n**********************************************\n\n");
+	ret = get_next_line(fd, line);
+	printf("\nRet de get_next_line: %d\n", ret);
 
 	close(fd);
 
