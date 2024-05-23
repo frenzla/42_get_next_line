b47388b35bd97f89543769f344cec5076ae3b8b4
frenzla <alex.larose.sf@gmail.com>
2024-05-23 18:04:06 +0200

    read_n_stock function done and works

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: alarose <alarose@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 09:29:14 by alarose           #+#    #+#             */
-/*   Updated: 2024/05/18 10:34:34 by alarose          ###   ########.fr       */
+/*   Updated: 2024/05/18 11:19:04 by alarose          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,67 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/*int	add_to_stock(char *buff, t_list **stock)
+//******************TEST FUNCTION******************
+void	print_lst(t_list **stock)
 {
+	t_list	*tmp;
 
-}*/
+	tmp = *stock;
+	if (!tmp)
+		printf("STOCK IS EMPTY\n");
+	else
+	{
+		printf("IN STOCK: ");
+		while (tmp)
+		{
+			printf("%c|", tmp->c);
+			tmp = tmp->next;
+		}
+		printf("\n");
+	}
+}
+//******************TEST FUNCTION******************
+
+int	add_to_stock(char c, t_list **stock)
+{
+	t_list	*new;
+	t_list	*tmp;
+
+	new = malloc(sizeof(t_list));
+	if (!new)
+		return (-1);
+	new->c = c;
+	new->next = NULL;
+	if (!*stock)
+		*stock = new;
+	else
+	{
+		tmp = *stock;
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = new;
+	}
+	return (0);
+}
 
 size_t	read_n_stock(int fd, char *buff)
 {
 	size_t	ret_read;
 	t_list	*stock;
+	int		i;
 
-	ret_read = -1;
 	ret_read = read(fd, buff, BUFFER_SIZE);
 	if (ret_read < 1)
 		return (ret_read);
 	buff[ret_read] = '\0';
 	//WORKING: printf("ret_read: %zu, buff: %s\n", ret_read, buff);
-
-	//add_to_stock(buff, &stock);
-
+	i = 0;
+	while (i < ret_read)
+	{
+		add_to_stock(buff[i], &stock);
+		i++;
+	}
+	//WORKING: print_lst(&stock);
 	return(ret_read);
 }
 
@@ -50,7 +92,9 @@ char	*get_next_line(int fd)
 		ret_read = read_n_stock(fd, buff);
 		if (ret_read < 1)
 			return (NULL);
+		//WORKING: printf("\nRET_READ = %zu\n\n", ret_read);
 		//check stock for newline OR EOF
+
 	}
 	//count nb_chars to write in line
 
--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,16 @@
 /*   By: alarose <alarose@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 10:47:59 by alarose           #+#    #+#             */
-/*   Updated: 2024/05/07 14:55:55 by alarose          ###   ########.fr       */
+/*   Updated: 2024/05/18 11:12:17 by alarose          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUFFER_SIZE
-#define BUFFER_SIZE -1
+# define BUFFER_SIZE 10
 #endif
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # include <stdlib.h>
 # include <unistd.h>
